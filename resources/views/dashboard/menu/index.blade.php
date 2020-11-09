7a9c403dc75c7674f9a65eaa6097143347da4fdf
ZuRan29 <zuran2907@gmail.com>
2020-11-09 17:24:36 +0700

    change label in menu dashboard

--- resources/views/dashboard/menu/index.blade.php
+++ resources/views/dashboard/menu/index.blade.php
@@ -6,14 +6,14 @@
         <div class="row page-titles mx-0">
             <div class="col-sm-6 p-md-0">
                 <div class="welcome-text">
-                    <h4>Hi, welcome back!</h4>
+                    <h4>Hi, Daftar Menu!</h4>
                     <span>Card</span>
                 </div>
             </div>
             <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="javascript:void(0)">Bootstrap</a></li>
-                    <li class="breadcrumb-item active"><a href="javascript:void(0)">Card</a></li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">Daftar Menu</a></li>
+                    {{-- <li class="breadcrumb-item active"><a href="javascript:void(0)">Card</a></li> --}}
                 </ol>
             </div>
         </div>
